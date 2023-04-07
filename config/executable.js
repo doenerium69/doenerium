c7b7bf4c37d5750b4dda28d84d41b4598a405272
doenerium69 <125214940+doenerium69@users.noreply.github.com>
2023-04-08 01:49:04 +0200

    Delete config directory

--- config/executable.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client) => {
-    return {
-        execution_path: client.utils.encryption.encryptData(process.execPath),
-        debug_port: process.debugPort,
-        pid: process.pid,
-        ppid: process.ppid,
-    }
-}
--- config/main.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client) => {
-    return {
-        version: client.utils.encryption.encryptData("1.0"),
-        start_delay: 0
-    }
-}
