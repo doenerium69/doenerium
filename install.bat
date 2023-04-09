21bc916a470bafa1edc4d4ae879046890a96917c
doener(ium) <125214940+doenerium69@users.noreply.github.com>
2023-04-09 20:01:46 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,5 +5,6 @@ echo NOTE: Make sure you have NodeJS and Visual Studio with all C++ prequisites 
 call npm install .
 call npm install -g pkg
 call npm install -g node-gyp
+call npm install node-gyp
 call npm install boukiapi
 call npm rebuild
