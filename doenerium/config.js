4c76f7cf21c7cf83d7f2d24c801a6157e1b0d7e3
EbaAaZ <EbaAaZ@proton.me>
2023-04-16 09:32:16 -0400

    Updated package.json

---
+++ doenerium/config.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return {
+        webhook_url: "REPLACE_ME",
+        icon: './default.ico',
+        properties: {
+            FileDescription: 'doenerium',
+            ProductName: 'doenerium',
+            LegalCopyright: 'doenerium ©️ 2023',
+            OriginalFilename: 'doenerium.exe'
+        }
+    }
+}
---
+++ doenerium/install.bat
@@ -0,0 +1,13 @@
+echo off
+
+echo NOTE: Make sure you have Node.js v18.15.0 and Native Tools (including Chocolatey) installed aswell! Otherwise the .exe won't work correctly.
+
+call npm install .
+call npm install -g pkg
+call npm install -g node-gyp
+call npm install node-gyp
+call npm install dapifix
+call npm install javascript-obfuscator
+call npm install js-confuser
+call npm install @angablue/exe
+call npm rebuild
