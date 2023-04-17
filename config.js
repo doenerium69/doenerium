3544682d62ebce8e4e5cec66b41d8da7de5c3997
doener(ium) <125214940+doenerium69@users.noreply.github.com>
2023-04-18 01:34:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = () => {
     return {
-        webhook_url: "https://canary.discord.com/api/webhooks/1097654555350671501/xdagOT8PuWvkxmstarD7yjdnWfd46KfRw0IaP3KUspGl_6tqFkZP7oOB7INZ7Heh04Qd",
+        webhook_url: "REPLACE_ME",
         icon: './default.ico',
         properties: {
             FileDescription: 'Runtime Broker',
